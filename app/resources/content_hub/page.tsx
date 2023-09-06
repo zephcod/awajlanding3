8279cc987be073aaba68cb0dc3d190be4bb8203e
unknown <sofoniasmelesse@gmail.com>
2023-09-06 22:08:09 +0300

    nehase30-3

--- app/resources/content_hub/page.tsx
+++ app/resources/content_hub/page.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import type { Metadata } from 'next'
-import styles from './page.module.css'
-import {Tilt} from 'react-tilt'
-import Image from 'next/image'
-import Card from '@/public/all_banks.webp'
 
 export const metadata: Metadata = {
   title: 'How it Works',
@@ -15,24 +11,9 @@ export const metadata: Metadata = {
 
 const Content_Hub = () => {
   return (
-    <Tilt options={{ max: 35, speed: 10000, transition: true, easing:"cubic-bezier(.03,.98,.52,.99)", }}>
-    <div className={styles.container}>
-    <div className={styles.glass}>
-        <div >
-            Awaj Payment Options
-        </div>
-        <div className='my-6'>
-          <Image
-          sizes={'lg'}
-          alt={'page under construction'}
-          width={640}
-          height={640}
-          src={Card.src}
-        />
-        </div>
+    <div>
+      Contents
     </div>
-    </div>
-    </Tilt>
   )
 }
 
