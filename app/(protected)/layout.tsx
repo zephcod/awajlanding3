c132fd9724ed3c9a6607475ceafc933ca2a53c3c
unknown <sofoniasmelesse@gmail.com>
2023-12-07 18:05:07 +0300

    hidar27b

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,17 +1,27 @@
 'use client'
-import React, { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import useAuth from '@/hooks/use_auth'
+import { toast } from 'sonner'
 
 const Layout = ({ children }: { children: ReactNode }) =>{
-    // const router = useRouter()
-    // const {authStatus} = useAuth()
+  const router = useRouter()
+  const user = useAuth().authStatus
+  const [loadingUser, setLoadingUser] = useState(false)
 
-    // if (!authStatus) {
-    //     router.push('/signin')
-    //     return<></>
-    // }
-  return children
+  useEffect(() => {
+    if (!user) {
+      // await delay(10000)
+        setLoadingUser(false)
+        router.push('/');
+        toast.error('You must be signed in to continue.')
+    }
+  }, [])
+
+
+    if (user) {
+      return children
+  }
 }
 
 export default Layout
