60caa1e354bc63e21b8e5204fdab7f12cd7aceaf
unknown <sofoniasmelesse@gmail.com>
2023-12-07 18:13:31 +0300

    hidar27c

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -5,9 +5,9 @@ import NavName from '@/components/dashboard/sidenav_name'
 const Layout = async ({ children }: { children: ReactNode }) =>{
   return (
     <div className="w-full pt-14">
-        <Suspense fallback = {<DashNav name = ''/>}>
+        {/* <Suspense fallback = {<DashNav name = ''/>}>
         {await NavName()}
-        </Suspense>
+        </Suspense> */}
         <div className="lg:pl-72">
               {children}
         </div>
